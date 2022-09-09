8232648dbe100e4ced70956c2af87c08878b3844
Ocksana Bondarenko <bondarrren777@gmail.com>
2022-09-09 16:17:57 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  textarea: document.querySelector('.feedback-form textarea'),
+};
 
+refs.form.addEventListener('submit', onFormSubmit);
+refs.textarea.addEventListener('input', onTexareaInput);
+
+function onFormSubmit(evt) {}
+
+function onTexareaInput(evt) {}
+const value = evt.currentTarget.value;
+console.log(value);
