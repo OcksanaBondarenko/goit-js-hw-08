b446d48e3815ae8f4bcb801fd6bf2a4d2f64a202
OcksanaBondarenko <97797043+OcksanaBondarenko@users.noreply.github.com>
2022-09-13 08:03:13 +0000

    Deploying to gh-pages from @ OcksanaBondarenko/goit-js-hw-08@8232648dbe100e4ced70956c2af87c08878b3844 🚀

---
+++ 03-feedback.4a5ed47c.js
@@ -0,0 +1,2 @@
+!function(){var e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){}));var t=evt.currentTarget.value;console.log(t)}();
+//# sourceMappingURL=03-feedback.4a5ed47c.js.map
---
+++ 03-feedback.c8dac301.js
@@ -0,0 +1,2 @@
+const e={form:document.querySelector(".feedback-form"),textarea:document.querySelector(".feedback-form textarea")};e.form.addEventListener("submit",(function(e){})),e.textarea.addEventListener("input",(function(e){}));const t=evt.currentTarget.value;console.log(t);
+//# sourceMappingURL=03-feedback.c8dac301.js.map
