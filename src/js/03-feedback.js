83966e622fb9c60e877b09f6471c4732d9e88230
Ocksana Bondarenko <bondarrren777@gmail.com>
2023-05-02 15:31:34 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,16 +2,15 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('form');
 const STORAGE_KEY = 'feedback-form-state';
-const LOCALSTORAGE_KEY = localStorage.getItem(STORAGE_KEY);
-
-form.addEventListener('submit', throttle(onSubmitClick, 500));
-form.addEventListener('input', inputIracking);
+const getStorage = localStorage.getItem(STORAGE_KEY);
+let formData = getStorage ? JSON.parse(getStorage) : {};
+// form.addEventListener('submit', throttle(onSubmitClick, 500));
+form.addEventListener('submit', onSubmitClick);
+form.addEventListener('input', _.throttle(inputTracking, 500));
 
 checkStorage();
 
-function inputIracking(event) {
-  let formData = LOCALSTORAGE_KEY;
-  formData = formData ? JSON.parse(formData) : {};
+function inputTracking(event) {
   formData[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 }
@@ -19,20 +18,26 @@ function inputIracking(event) {
 function onSubmitClick(event) {
   event.preventDefault();
 
-  console.log(`formData: `, JSON.parse(LOCALSTORAGE_KEY));
+  const email = form.elements.email.value;
+  const message = form.elements.message.value;
 
-  event.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+  if (email === '' || message === '') {
+    alert(`Усі поля форми мають бути заповнені!`);
+  } else {
+    console.log(`formData: `, JSON.parse(getStorage));
+
+    event.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
 }
 
 function checkStorage() {
-  if (localStorage.getItem(STORAGE_KEY)) {
-    let data = LOCALSTORAGE_KEY;
-    const formDataParse = JSON.parse(LOCALSTORAGE_KEY);
-    if (data) {
-      data = JSON.parse(data);
-      form.elements.email.value = formDataParse.email;
-      form.elements.message.value = formDataParse.message;
-    }
+  let data = localStorage.getItem(STORAGE_KEY);
+  if (data) {
+    const formDataParse = JSON.parse(getStorage);
+    form.elements.email.value = formDataParse.email ? formDataParse.email : '';
+    form.elements.message.value = formDataParse.message
+      ? formDataParse.message
+      : '';
   }
 }
